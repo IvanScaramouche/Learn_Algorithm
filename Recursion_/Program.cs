e95ef6f106262c07f717da62e93c2564b71cdad5
IvanScaramouche <ivan.skaram@ya.ru>
2020-07-23 23:00:09 +0700

    Recursion

---
+++ Recursion_/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Recursion_ {
+    class Program {
+        static void Main() {
+            Console.WriteLine($"Факторияла из числа 8: {Recursion.Factorial(8)}");
+            Console.WriteLine();
+        }
+    }
+}
